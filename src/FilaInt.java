b0b0a8957d1b872e39cbbb05ca4ff0fd8f84d6bc
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-06 11:29:57 -0300

    rascunho exercicio 1 partes 1 e 2

--- src/FilaInt.java
+++ src/FilaInt.java
@@ -1,2 +1,45 @@
+import java.util.Scanner;
+
 public class FilaInt {
+    final int N = 3;
+    int[] queue = new int[N];
+    int head, tail, index;
+
+    public void init() {
+        index = tail = 0;
+    }
+
+    public boolean isEmpty() {
+        return (index == 0);
+    }
+
+    public boolean isFull() {
+        return (index == N);
+    }
+
+    public void enqueue(int elem) {
+        if (isFull()) {
+            System.out.println("[FULL QUEUE]");
+        } else {
+            queue[tail] = elem;
+            index++;
+//            tail++;
+
+//            if (tail == N) {
+//                tail = 0;
+//            }
+            tail = (tail + 1) % N;
+        }
+    }
+
+    public int dequeue() {
+        int elem = queue[head];
+        index--;
+//        if (head == N) {
+//            head = 0;
+//        }
+        head = (head - 1) % N;
+        return elem;
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    static Scanner inp = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        FilaInt filaInt = new FilaInt();
+        int rm;
+
+
+        filaInt.init();
+        System.out.println("RM do aluno:    ");
+        rm = inp.nextInt();
+
+        if (filaInt.isEmpty()) {
+            System.out.println("[FULL QUEUE]");
+        } else {
+            System.out.println("Valor retirado: " + filaInt.dequeue());
+        }
+
+
+        while (!filaInt.isEmpty()) {
+            System.out.println("Valor retirado: " + filaInt.dequeue());
+        }
+    }
 }
